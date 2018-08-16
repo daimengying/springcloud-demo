f946f405297c321ffacec57ba333dfd38c9ad6b8
mydai <1922159124@qq.com>
2018-08-16 12:09:28 +0800

    springcloud第三次提交，配置中心对称加密

--- server-api/src/main/java/com/dmy/springcloud/serverapi/apirest/TestApi.java
+++ server-api/src/main/java/com/dmy/springcloud/serverapi/apirest/TestApi.java
@@ -1,7 +1,9 @@
 package com.dmy.springcloud.serverapi.apirest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,14 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
  * @Description:API对外暴露服务测试类
  */
 @RestController
+@RefreshScope
 public class TestApi {
     @Autowired
     DiscoveryClient discoveryClient;
 
+    @Value("${info.name}")
+    private String infoName;
+    @Value("${str}")
+    private String str;
+
     @GetMapping("/dc")
     public String dc() {
-        String services = "Services: " + discoveryClient.getServices();
-        System.out.println(services);
-        return services;
+//        String services = "Services: " + discoveryClient.getServices();
+//        System.out.println(services);
+        return infoName+"-----"+str;
     }
 }
